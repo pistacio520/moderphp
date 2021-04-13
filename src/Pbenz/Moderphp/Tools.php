e672a45efa491ced484349cdc118d66a39dff842
义乌小毛毛 <46677126@qq.com>
2021-04-13 22:26:35 +0800

    我的第一包

---
+++ src/Pbenz/Moderphp/Tools.php
@@ -0,0 +1,14 @@
+<?php
+namespace Pbenz\Moderphp;
+
+class Tools
+{
+ public function __construct()
+ {
+
+ }
+ public static function Say()
+ {
+  echo "OY,This is my first component of php";
+ }
+}
